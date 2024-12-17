3d40ec23c4454519bb1018ac14bf29419a5ac94c
Elemite Intro <113902356+incrideable@users.noreply.github.com>
2024-12-17 20:23:44 +0800

    Delete LMS directory

--- LMS/web/view/loginOut.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from function.userDao.user_online import user_Outline
-
-
-def loginOut(request):
-    user_Outline()
-    return render(request,'login_new.html')
